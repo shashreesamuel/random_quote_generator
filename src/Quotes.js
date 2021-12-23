99fe2763c503fc1d96500d428035902e4a1cff76
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-12-23 09:16:37 -0400

    Updated the app.js file

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,23 +1,7 @@
 import { quotes } from "./quotesdata";
-import React from "react";
 
 
 export let getRandomQuote = () => {
 
 	return( quotes[ Math.floor( Math.random() * ( quotes.length + 1 ) ) ][ "quote" ] + ' ~ ' + quotes[ Math.floor( Math.random() * ( quotes.length + 1 ) ) ][ "author" ] );
 } 
-
-// export const Quotes = () => {
-//     return(
-//         <>
-//         {quotes.map((data, key) => {
-//             return (
-//                 <div key={key}>
-//                     {data[ "quote" ]}
-//                     {getRandomQuote}
-//                 </div>
-//             )
-//         })}
-//         </>
-//     )
-// }
