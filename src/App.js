225ca0c2acf40e7f0700bd93deb6e7f76c8e9f6c
Shash <86580705+TheCoderGuru@users.noreply.github.com>
2022-01-21 19:19:39 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-import { getRandomQuote } from './Quotes';
-import React, { useState } from 'react';
-
+import { getRandomQuote, Quotes } from './Quotes';
+import { useState } from 'react';
+import DarkModeToggle from "react-dark-mode-toggle";
 
 const App = () => {
 
   const [ quote, setQuote ] = useState()
+
+  const [ darkmode, setDarkMode ] = useState(false)
+
+  const handleDark = () => {
+    setDarkMode(true);
+    console.log(darkmode)
+  }
+
+  const quotes = () => {
+
+    getRandomQuote()
+
+  }
+
+  const handleText = () => {
+    return 'Here is your quote'
+  }
 
   const setRandomQuote = () => {
 
@@ -19,8 +36,8 @@ const App = () => {
         <p class={!quote ? "toggleStyle": "notToggleStyle"}>A random quote generator designed to empower and inspire you with famous quotes from intelligent and well known scholars. <br />Anyday, Anytime</p>
         <br />
         <br />
+        {darkmode ? <body className="darkMode"></body> : ''}
         <p class={quote ? "generatedQuote" : ''}>{quote}</p>
-  
         <button onClick={setRandomQuote} className="cta-button">Generate</button>
     </div>
   );
--- src/Quotes.js
+++ src/Quotes.js
@@ -1,7 +1,23 @@
 import { quotes } from "./quotesdata";
+import React from "react";
 
 
 export let getRandomQuote = () => {
 
 	return( quotes[ Math.floor( Math.random() * ( quotes.length + 1 ) ) ][ "quote" ] + ' ~ ' + quotes[ Math.floor( Math.random() * ( quotes.length + 1 ) ) ][ "author" ] );
 } 
+
+// export const Quotes = () => {
+//     return(
+//         <>
+//         {quotes.map((data, key) => {
+//             return (
+//                 <div key={key}>
+//                     {data[ "quote" ]}
+//                     {getRandomQuote}
+//                 </div>
+//             )
+//         })}
+//         </>
+//     )
+// }
